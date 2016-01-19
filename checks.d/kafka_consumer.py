865b87f9cc40fd9dd009f90dede92f7608c9cc4f
William Dowling <william.dowling@lookout.com>
2016-01-19 11:19:13 +0100

    Add option to specify spout for Kafka

--- checks.d/kafka_consumer.py
+++ checks.d/kafka_consumer.py
@@ -1,63 +1,75 @@
 # stdlib
 from collections import defaultdict
+import random
+import json
 
-# 3p
+# project
+from checks import AgentCheck
+
+# 3rd party
 from kafka.client import KafkaClient
 from kafka.common import OffsetRequest
 from kazoo.client import KazooClient
 from kazoo.exceptions import NoNodeError
 
-# project
-from checks import AgentCheck
-
-DEFAULT_KAFKA_TIMEOUT = 5
-DEFAULT_ZK_TIMEOUT = 5
-
-
 class KafkaCheck(AgentCheck):
 
     SOURCE_TYPE_NAME = 'kafka'
 
-    def __init__(self, name, init_config, agentConfig, instances=None):
-        AgentCheck.__init__(self, name, init_config, agentConfig, instances=instances)
-        self.zk_timeout = int(
-            init_config.get('zk_timeout', DEFAULT_ZK_TIMEOUT))
-        self.kafka_timeout = int(
-            init_config.get('kafka_timeout', DEFAULT_KAFKA_TIMEOUT))
-
     def check(self, instance):
-        consumer_groups = self.read_config(instance, 'consumer_groups',
-                                           cast=self._validate_consumer_groups)
+	znode = 'znode'
+        consumer_groups = self.read_config(instance, 'consumer_groups')
+#                                           cast=self._validate_consumer_groups)
         zk_connect_str = self.read_config(instance, 'zk_connect_str')
         kafka_host_ports = self.read_config(instance, 'kafka_connect_str')
-
         # Construct the Zookeeper path pattern
         zk_prefix = instance.get('zk_prefix', '')
         zk_path_tmpl = zk_prefix + '/consumers/%s/offsets/%s/%s'
 
         # Connect to Zookeeper
-        zk_conn = KazooClient(zk_connect_str, timeout=self.zk_timeout)
+        zk_conn = KazooClient(zk_connect_str)
         zk_conn.start()
-
         try:
             # Query Zookeeper for consumer offsets
             consumer_offsets = {}
             topics = defaultdict(set)
-            for consumer_group, topic_partitions in consumer_groups.iteritems():
-                for topic, partitions in topic_partitions.iteritems():
-                    # Remember the topic partitions that we've see so that we can
-                    # look up their broker offsets later
-                    topics[topic].update(set(partitions))
-                    for partition in partitions:
-                        zk_path = zk_path_tmpl % (consumer_group, topic, partition)
-                        try:
-                            consumer_offset = int(zk_conn.get(zk_path)[0])
-                            key = (consumer_group, topic, partition)
-                            consumer_offsets[key] = consumer_offset
-                        except NoNodeError:
-                            self.log.warn('No zookeeper node at %s' % zk_path)
-                        except Exception:
-                            self.log.exception('Could not read consumer offset from %s' % zk_path)
+            for consumer_group in consumer_groups.iteritems():
+		consumer = consumer_group[0]
+		if znode in consumer_group[1]:
+		    zn = consumer_group[1]['znode']
+		else:
+		    zn = 'consumers'
+		for k,v in consumer_group[1].iteritems():
+		    if k != znode:
+			topics[k].update(set(v))
+
+		for topic in topics.iteritems():
+		    if topic[0] in consumer_group[1]:
+			if zn == 'consumers':
+				for partition in topic[1]:
+				    zk_path = zk_path_tmpl % (consumer, topic[0], partition)
+				    try:
+					consumer_offset = int(zk_conn.get(zk_path)[0])
+					key = (consumer, topic[0], partition)
+					consumer_offsets[key] = consumer_offset
+				    except NoNodeError:
+					self.log.warn('No zookeeper node at %s' % zk_path)
+				    except Exception:
+					self.log.exception('Could not read consumer offset from %s' % zk_path)
+			else:
+				for partition in topic[1]:
+				    part = 'partition_%s' % partition
+				    zk_path = '/%s/%s/%s' % (zn, consumer, part)
+				    try:
+					consumer_offset_json = json.loads(zk_conn.get(zk_path)[0])
+					consumer_offset = consumer_offset_json['offset']
+					key = (consumer, topic[0], partition)
+					consumer_offsets[key] = consumer_offset
+                                    except NoNodeError:
+                                        self.log.warn('No zookeeper node at %s' % zk_path)
+                                    except Exception:
+                                        self.log.exception('Could not read consumer offset from %s' % zk_path)
+
         finally:
             try:
                 zk_conn.stop()
@@ -66,7 +78,7 @@ class KafkaCheck(AgentCheck):
                 self.log.exception('Error cleaning up Zookeeper connection')
 
         # Connect to Kafka
-        kafka_conn = KafkaClient(kafka_host_ports, timeout=self.kafka_timeout)
+        kafka_conn = KafkaClient(kafka_host_ports)
 
         try:
             # Query Kafka for the broker offsets
@@ -74,9 +86,9 @@ class KafkaCheck(AgentCheck):
             for topic, partitions in topics.items():
                 offset_responses = kafka_conn.send_offset_request([
                     OffsetRequest(topic, p, -1, 1) for p in partitions])
-
                 for resp in offset_responses:
                     broker_offsets[(resp.topic, resp.partition)] = resp.offsets[0]
+
         finally:
             try:
                 kafka_conn.close()
@@ -94,7 +106,6 @@ class KafkaCheck(AgentCheck):
 
             # Get the broker offset
             broker_offset = broker_offsets.get((topic, partition))
-
             # Report the consumer offset and lag
             tags = ['topic:%s' % topic, 'partition:%s' % partition,
                     'consumer_group:%s' % consumer_group]
@@ -106,11 +117,12 @@ class KafkaCheck(AgentCheck):
 
     def _validate_consumer_groups(self, val):
         try:
-            consumer_group, topic_partitions = val.items()[0]
+            consumer_group, topic_partitions, consumer_path = val.items()[0]
             assert isinstance(consumer_group, (str, unicode))
             topic, partitions = topic_partitions.items()[0]
             assert isinstance(topic, (str, unicode))
             assert isinstance(partitions, (list, tuple))
+	    assert isinstance(consumer_path, (str, unicode))
             return val
         except Exception, e:
             self.log.exception(e)
